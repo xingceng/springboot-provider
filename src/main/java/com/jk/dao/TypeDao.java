35589e7174a68faee0f9a460211b21054c181bb9
MI <1394614013@qq.com>
2019-02-23 09:00:28 +0800

    博客类别

--- src/main/java/com/jk/dao/TypeDao.java
+++ src/main/java/com/jk/dao/TypeDao.java
@@ -10,7 +10,7 @@ public interface TypeDao {
     @Select("select count(*) from t_blogtype")
     long queryTypeTotal();
 
-    @Select("select * from t_blogtype limit #{start},#{rows}")
+    @Select("select bt.id,bt.typeName,bt.orderNum,count(*) blogCount from t_blog b,t_blogtype bt where b.type_id=bt.id group by bt.typeName limit #{start},#{rows}")
     List<BlogType> queryTypePage(@Param("start") int start, @Param("rows") int rows);
 
     @Insert("insert into t_blogtype(id,typeName,orderNum) values(#{id},#{typeName},#{orderNum})")
